b25ffe7a489d526e1f2a07129fd85a39ab0f89c9
Coder <coder@dev.lan>
2020-03-24 20:44:15 +0000

    use intention-revealing types

--- src/Skully.hs
+++ src/Skully.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE GADTs #-}
+
 module Skully (
     Skully(..)
 ) where
 
-data Skully a = S | K | U | L | Y | Q | Ap (Skully a) (Skully a)
+data Skully a where
+    S :: Skully ((a -> b -> c) -> (a -> b) -> a -> c)
+    K :: Skully (a -> b -> a)
+    U :: Skully (Char -> a -> a)
+    L :: Skully ((Char -> a) -> a)
+    Y :: Skully ((a -> a) -> a)
+    Q :: Skully (Char -> (Char -> Char -> a) -> a)
+    Ap :: Skully (a -> b) -> Skully a -> Skully b
 
 instance Show (Skully a) where
     show skully =
