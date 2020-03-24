b6e8261457f550d137d563856bd87a0b92a9b065
Coder <coder@dev.lan>
2020-03-24 20:35:05 +0000

    show u

--- src/Skully.hs
+++ src/Skully.hs
@@ -2,10 +2,11 @@ module Skully (
     Skully(..)
 ) where
 
-data Skully a = S | K
+data Skully a = S | K | U
 
 instance Show (Skully a) where
     show skully =
         case skully of
             S -> "s"
             K -> "k"
+            U -> "u"
--- test/TestSkully.hs
+++ test/TestSkully.hs
@@ -10,6 +10,7 @@ testShowSkully :: Spec
 testShowSkully = describe "show :: Skully a -> String" $ do
     it "prints S as \"s\"" $ show S `shouldBe` "s"
     it "prints K as \"k\"" $ show K `shouldBe` "k"
+    it "prints U as \"u\"" $ show U `shouldBe` "u"
 
 testSkully :: Spec
 testSkully = describe "operations on Skully a" $ do
