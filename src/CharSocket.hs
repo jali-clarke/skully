16e6c6af53f9a3b5c94c208369a005b9b096091b
Coder <coder@dev.lan>
2020-03-26 02:58:22 +0000

    IO instance for CharSocket

--- src/CharSocket.hs
+++ src/CharSocket.hs
@@ -2,8 +2,12 @@ module CharSocket (
     CharSocket(..)
 ) where
 
-import Prelude hiding (getChar, putChar)
+import Prelude as P
 
 class Monad m => CharSocket m where
     getChar :: m Char
     putChar :: Char -> m ()
+
+instance CharSocket P.IO where
+    getChar = P.getChar
+    putChar = P.putChar
