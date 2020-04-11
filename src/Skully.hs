ce3e2eb23b8e2ce162ec646025536f497e2c991c
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-04-11 02:40:18 +0000

    move eval to Skully.Internal.Type

--- src/Skully.hs
+++ src/Skully.hs
@@ -16,57 +16,7 @@ module Skully (
     char
 ) where
 
-import Prelude hiding (getChar, putChar)
-
-import Skully.Internal.CharSocket
 import Skully.Internal.Type
-
-eval :: CharSocket m => Skully a -> m (Skully a)
-eval expr =
-    case expr of
-        Ap (Ap (Ap S abc) ab) a -> eval (Ap (Ap abc a) (Ap ab a))
-        Ap (Ap K a) _ -> eval a
-        Ap (Ap U c) a ->
-            case c of
-                Char x -> putChar x *> eval a
-                _ -> do
-                    c' <- eval c
-                    eval (Ap (Ap U c') a)
-        Ap L g -> do
-            x <- getChar
-            eval (Ap g (Char x))
-        Ap Y g -> eval (Ap g (Ap Y g))
-        Ap (Ap Q c) g ->
-            case c of
-                Char x ->
-                    let predChar = if x == '\x00' then x else pred x
-                        succChar = if x == '\xff' then x else succ x
-                    in eval (Ap (Ap g (Char predChar)) (Char succChar))
-                _ -> eval c >>= (\c' -> eval (Ap (Ap Q c') g))
-        Ap (Ap (Ap (Ap (Ap E c0) c1) a) b) c ->
-            case (c0, c1) of
-                (Char x0, Char x1) ->
-                    eval $ case x0 `compare` x1 of
-                        LT -> a
-                        EQ -> b
-                        GT -> c
-                _ -> do
-                    c0' <- eval c0
-                    c1' <- eval c1
-                    eval (Ap (Ap (Ap (Ap (Ap E c0') c1') a) b) c)
-        Ap S _ -> pure expr
-        Ap (Ap S _) _ -> pure expr
-        Ap K _ -> pure expr
-        Ap U _ -> pure expr
-        Ap Q _ -> pure expr
-        Ap E _ -> pure expr
-        Ap (Ap E _) _ -> pure expr
-        Ap (Ap (Ap E _) _) _ -> pure expr
-        Ap (Ap (Ap (Ap E _) _) _) _ -> pure expr
-        Ap a b -> do
-            a' <- eval a
-            eval (Ap a' b)
-        _ -> pure expr
 
 s :: Skully ((a -> b -> c) -> (a -> b) -> a -> c)
 s = S
--- src/Skully/Internal/Type.hs
+++ src/Skully/Internal/Type.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE GADTs #-}
 
 module Skully.Internal.Type (
-    Skully(..)
+    Skully(..),
+    eval
 ) where
 
 import Prelude hiding (getChar, putChar)
 import Data.Char (chr, ord)
+
+import Skully.Internal.CharSocket
 
 data Skully a where
     S :: Skully ((a -> b -> c) -> (a -> b) -> a -> c)
@@ -18,11 +21,8 @@ data Skully a where
     Ap :: Skully (a -> b) -> Skully a -> Skully b
     Char :: Char -> Skully Char
 
-showHex :: Int -> (String -> String)
-showHex n =
-    let (d, m) = n `divMod` 16
-        toHexChar x = if x < 10 then (chr (x + 48) :) else (chr (x + 87) :)
-    in toHexChar d . toHexChar m
+instance Show (Skully a) where
+    show skully = show' skully ""
 
 show' :: Skully a -> (String -> String)
 show' skully =
@@ -45,5 +45,55 @@ show' skully =
                         else (c :)
             in ('\'' :) . showCharAsHexIfNecessary . ('\'' :)
 
-instance Show (Skully a) where
-    show skully = show' skully ""
+showHex :: Int -> (String -> String)
+showHex n =
+    let (d, m) = n `divMod` 16
+        toHexChar x = if x < 10 then (chr (x + 48) :) else (chr (x + 87) :)
+    in toHexChar d . toHexChar m
+
+eval :: CharSocket m => Skully a -> m (Skully a)
+eval expr =
+    case expr of
+        Ap (Ap (Ap S abc) ab) a -> eval (Ap (Ap abc a) (Ap ab a))
+        Ap (Ap K a) _ -> eval a
+        Ap (Ap U c) a ->
+            case c of
+                Char x -> putChar x *> eval a
+                _ -> do
+                    c' <- eval c
+                    eval (Ap (Ap U c') a)
+        Ap L g -> do
+            x <- getChar
+            eval (Ap g (Char x))
+        Ap Y g -> eval (Ap g (Ap Y g))
+        Ap (Ap Q c) g ->
+            case c of
+                Char x ->
+                    let predChar = if x == '\x00' then x else pred x
+                        succChar = if x == '\xff' then x else succ x
+                    in eval (Ap (Ap g (Char predChar)) (Char succChar))
+                _ -> eval c >>= (\c' -> eval (Ap (Ap Q c') g))
+        Ap (Ap (Ap (Ap (Ap E c0) c1) a) b) c ->
+            case (c0, c1) of
+                (Char x0, Char x1) ->
+                    eval $ case x0 `compare` x1 of
+                        LT -> a
+                        EQ -> b
+                        GT -> c
+                _ -> do
+                    c0' <- eval c0
+                    c1' <- eval c1
+                    eval (Ap (Ap (Ap (Ap (Ap E c0') c1') a) b) c)
+        Ap S _ -> pure expr
+        Ap (Ap S _) _ -> pure expr
+        Ap K _ -> pure expr
+        Ap U _ -> pure expr
+        Ap Q _ -> pure expr
+        Ap E _ -> pure expr
+        Ap (Ap E _) _ -> pure expr
+        Ap (Ap (Ap E _) _) _ -> pure expr
+        Ap (Ap (Ap (Ap E _) _) _) _ -> pure expr
+        Ap a b -> do
+            a' <- eval a
+            eval (Ap a' b)
+        _ -> pure expr
