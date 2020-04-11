b3fc2b6a4ad28bc0b80ad2886522beaa4496db41
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-04-11 04:44:55 +0000

    add tests for a-exprs

--- test/TestSkully/Stdlib/Combinators.hs
+++ test/TestSkully/Stdlib/Combinators.hs
@@ -8,19 +8,30 @@ import Helpers
 import Skully.Base
 import Skully.Stdlib.Combinators
 
+testEvalA :: Spec
+testEvalA =
+    describe "eval-ing a exprs" $ do
+        it "applies its second argument to its first in a .$ char 'u' .$ k .$ l" $
+            withStreamsShouldReturn ("", "") ("'u'", ("", "")) $
+                a .$ char 'u' .$ k .$ l
+        it "evaluates its result in a .$ s .$ (u .$ char 'j')" $
+            withStreamsShouldReturn ("", "") ("s", ("", "j")) $
+                a .$ s .$ (u .$ char 'j')
+
 testEvalI :: Spec
 testEvalI =
     describe "eval-ing i exprs" $ do
         it "returns its argument in i .$ u" $
             withStreamsShouldReturn ("", "") ("u", ("", "")) $
                 i .$ u
-        it "evaluates its argument in i .$ (k .$ char 'x' .$ s)" $
+        it "evaluates its result in i .$ (k .$ char 'x' .$ s)" $
             withStreamsShouldReturn ("", "") ("'x'", ("", "")) $
                 i .$ (k .$ char 'x' .$ s)
 
 testEvalSkully :: Spec
 testEvalSkully =
     describe "eval :: CharSocket m => Skully a -> m (Skully a)" $ do
+        testEvalA
         testEvalI
 
 testSkullyCombinators :: Spec
