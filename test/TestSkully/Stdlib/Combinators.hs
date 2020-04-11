735df2b82952c12c3f78e1fceeb861bf2a548450
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-04-11 04:41:17 +0000

    add tests for i

--- test/TestSkully/Stdlib/Combinators.hs
+++ test/TestSkully/Stdlib/Combinators.hs
@@ -3,8 +3,27 @@ module TestSkully.Stdlib.Combinators (
 ) where
 
 import Test.Hspec
+import Helpers
+
+import Skully.Base
+import Skully.Stdlib.Combinators
+
+testEvalI :: Spec
+testEvalI =
+    describe "eval-ing i exprs" $ do
+        it "returns its argument in i .$ u" $
+            withStreamsShouldReturn ("", "") ("u", ("", "")) $
+                i .$ u
+        it "evaluates its argument in i .$ (k .$ char 'x' .$ s)" $
+            withStreamsShouldReturn ("", "") ("'x'", ("", "")) $
+                i .$ (k .$ char 'x' .$ s)
+
+testEvalSkully :: Spec
+testEvalSkully =
+    describe "eval :: CharSocket m => Skully a -> m (Skully a)" $ do
+        testEvalI
 
 testSkullyCombinators :: Spec
 testSkullyCombinators =
     describe "Skully.Stdlib.Combinators" $ do
-        pure ()
+        testEvalSkully
