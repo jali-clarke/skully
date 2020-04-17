a5be6644b3357789783212d9e0f2ac3de35e4f8d
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-04-17 04:02:09 +0000

    we can now reverse lists lol

--- src/Skully/Stdlib/Containers/List.hs
+++ src/Skully/Stdlib/Containers/List.hs
@@ -3,20 +3,30 @@ module Skully.Stdlib.Containers.List (
 
     nil,
     cons,
+    reverse,
 
     withList
 ) where
+
+import Prelude hiding (reverse)
 
 import Skully.Base
 import Skully.Stdlib.Combinators
 
 type List b a = b -> (a -> b -> b) -> b
+type DiffList b a = List b a -> List b a
 
 nil :: Skully (List b a)
 nil = k
 
 cons :: Skully (a -> List b a -> List b a)
 cons = c .$ c .$ (c .$ s .$ a)
+
+reverseDiff :: Skully (List (DiffList b a) a -> DiffList b a)
+reverseDiff = f .$ (a .$ i) .$ (c .$ (f .$ c) .$ cons)
+
+reverse :: Skully (List (DiffList b a) a -> List b a)
+reverse = f .$ reverseDiff .$ nil
 
 withList :: Skully (List b a -> b -> (a -> b -> b) -> b)
 withList = i
--- test/TestSkully/Stdlib/Containers/List.hs
+++ test/TestSkully/Stdlib/Containers/List.hs
@@ -2,6 +2,8 @@ module TestSkully.Stdlib.Containers.List (
     testSkullyList
 ) where
 
+import Prelude hiding (reverse)
+
 import Test.Hspec
 import Helpers
 
@@ -22,9 +24,23 @@ testSkullyListCons =
             withStreamsShouldReturn ("", "") ("k", ("", "xy")) $
                 withList .$ (cons .$ char 'x' .$ (cons .$ char 'y' .$ nil)) .$ k .$ u
 
+testSkullyListReverse :: Spec
+testSkullyListReverse =
+    describe "withList on reversed list" $ do
+        it "does nothing if list is empty" $
+            withStreamsShouldReturn ("", "") ("k", ("", "")) $
+                withList .$ (reverse .$ nil) .$ k .$ u
+        it "prints single element in withList .$ (reverse .$ (cons .$ char 'a' .$ nil)) .$ k .$ u" $
+            withStreamsShouldReturn ("", "") ("k", ("", "a")) $
+                withList .$ (reverse .$ (cons .$ char 'a' .$ nil)) .$ k .$ u
+        it "prints all elements in reversed list in withList .$ (reverse .$ (cons .$ char 'x' .$ (cons .$ char 'y' .$ nil))) .$ k .$ u" $
+            withStreamsShouldReturn ("", "") ("k", ("", "yx")) $
+                withList .$ (reverse .$ (cons .$ char 'x' .$ (cons .$ char 'y' .$ nil))) .$ k .$ u
+
 testSkullyList :: Spec
 testSkullyList =
     describe "Skully.Stdlib.Containers.List" $ do
         describe "withList :: Skully (List b a -> b -> (a -> b -> b) -> b)" $ do
             testSkullyListNil
             testSkullyListCons
+            testSkullyListReverse
