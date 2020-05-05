b8428819f27441a34074755a9c04ce0e98bda929
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-05-05 13:09:54 +0000

    rework dir structure - add uppercase, move reverseLines from app into own dir, rename executables

---
+++ examples/uppercase/Main.hs
@@ -0,0 +1,48 @@
+import Prelude hiding (Bool, and)
+
+import Data.Functor (void)
+import Skully.Stdlib
+
+type Bool b = b -> b -> b
+
+true :: Skully (Bool b)
+true = k
+
+false :: Skully (Bool b)
+false = f .$ k
+
+ifB :: Skully (Bool b -> b -> b -> b)
+ifB = i
+
+dec :: Skully (Char -> Char)
+dec = f .$ q .$ k
+
+sub :: Int -> Skully (Char -> Char)
+sub n = if n < 1 then i else c .$ dec .$ sub (n - 1)
+
+capitalize :: Skully (Char -> Char)
+capitalize = sub 32
+
+and :: Skully (Bool b -> Bool b -> Bool b)
+and = c .$ (c .$ (c .$ d)) .$ (f .$ (c .$ c .$ (c .$ c .$ (c .$ c .$ ifB))) .$ ifB)
+
+isGreaterThanBacktick :: Skully (Char -> Bool b)
+isGreaterThanBacktick = f .$ (f .$ (f .$ (e .$ char '`') .$ true) .$ false) .$ false
+
+isLessThanLeftBrace :: Skully (Char -> Bool b)
+isLessThanLeftBrace = f .$ (f .$ (f .$ (e .$ char '{') .$ false) .$ false) .$ true
+
+isLowerCase :: Skully (Char -> Bool b)
+isLowerCase = s .$ (c .$ and .$ isGreaterThanBacktick) .$ isLessThanLeftBrace
+
+toUpperCase :: Skully (Char -> Char)
+toUpperCase = d .$ (s .$ (c .$ ifB .$ isLowerCase) .$ capitalize)
+
+step :: Skully (a -> a)
+step = c .$ l .$ (c .$ (f .$ c .$ toUpperCase) .$ (f .$ u))
+
+prog :: Skully s
+prog = y .$ step
+
+main :: IO ()
+main = void $ eval (optimize prog)
