3886ccba02acd162b8ab79a0a90f5096bc758207
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-05-15 20:58:07 +0000

    unification for all non-var types

--- src/Skully/Internal/TypeRep.hs
+++ src/Skully/Internal/TypeRep.hs
@@ -11,6 +11,7 @@ data TypeRep a where
 
 instance Eq (TypeRep a) where
     Char == Char = True
+    (Char :->: Char) == (Char :->: Char) = True
     _ == _ = False
 
 instance Show (TypeRep a) where
@@ -19,4 +20,14 @@ instance Show (TypeRep a) where
 
 unify :: TypeRep a -> TypeRep b -> Either String (TypeRep a)
 unify Char Char = Right Char
-unify _ _ = Left "cannot unify (Char :->: Char) against Char"
+unify (Char :->: Char) (Char :->: Char) = Right (Char :->: Char)
+unify a b =
+    let aString =
+            case a of
+                (_ :->: _) -> "(" ++ show a ++ ")"
+                _ -> show a
+        bString =
+            case b of
+                _ :->: _ -> "(" ++ show b ++ ")"
+                _ -> show b
+    in Left ("cannot unify " ++ aString ++ " against " ++ bString)
--- test/TestSkully/Internal/TypeRep.hs
+++ test/TestSkully/Internal/TypeRep.hs
@@ -12,6 +12,10 @@ testUnify =
         it "should unify Char against Char" $ unify Char Char `shouldBe` Right Char
         it "should not unify (Char :->: Char) against Char" $
             unify (Char :->: Char) Char `shouldBe` Left "cannot unify (Char :->: Char) against Char"
+        it "should unify (Char :->: Char) against (Char :->: Char)" $
+            unify (Char :->: Char) (Char :->: Char) `shouldBe` Right (Char :->: Char)
+        it "should not unify Char against (Char :->: Char)" $
+            unify Char (Char :->: Char) `shouldBe` Left "cannot unify Char against (Char :->: Char)"
 
 testSkullyTypeRep :: Spec
 testSkullyTypeRep =
