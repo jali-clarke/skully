441bb81aecbfc29e81d3fb256614d6f4a905752e
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-05-15 23:01:18 +0000

    change unification to keep track of a map of constraints

--- src/Skully/Internal/TypeRep.hs
+++ src/Skully/Internal/TypeRep.hs
@@ -1,26 +1,49 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE
+    GADTs,
+    RankNTypes
+#-}
 
 module Skully.Internal.TypeRep (
     TypeRep(..),
+    SomeTypeRep(..),
+    TypeMap,
+
     unify
 ) where
+
+import qualified Data.IntMap.Strict as Map
 
 data TypeRep a where
     Char :: TypeRep Char
     (:->:) :: TypeRep a -> TypeRep b -> TypeRep (a -> b)
+    Var :: Int -> TypeRep a
+
+data SomeTypeRep = forall a. SomeTypeRep (TypeRep a)
+type TypeMap = Map.IntMap SomeTypeRep
 
 instance Eq (TypeRep a) where
     Char == Char = True
     (Char :->: Char) == (Char :->: Char) = True
+    Var n0 == Var n1 = n0 == n1
     _ == _ = False
 
 instance Show (TypeRep a) where
     show a = show' a ""
 
+instance Eq SomeTypeRep where
+    (SomeTypeRep a) == (SomeTypeRep b) =
+        case unify a b Map.empty of
+            Left _ -> False
+            Right _ -> True
+
+instance Show SomeTypeRep where
+    show (SomeTypeRep a) = show a
+
 show' :: TypeRep a -> (String -> String)
 show' a =
     case a of
         Char -> ("Char" ++)
+        Var n -> ('a' :) . (show n ++)
         _ -> ("Char :->: Char" ++)
 
 nestedShow :: TypeRep a -> (String -> String)
@@ -29,7 +52,10 @@ nestedShow a =
         (_ :->: _) -> ('(' :) . show' a . (')' :)
         _ -> show' a
 
-unify :: TypeRep a -> TypeRep b -> Either String (TypeRep a)
-unify Char Char = Right Char
-unify (Char :->: Char) (Char :->: Char) = Right (Char :->: Char)
-unify a b = Left (("cannot unify " ++) . nestedShow a . (" against " ++) . nestedShow b $ "")
+unify :: TypeRep a -> TypeRep b -> TypeMap -> Either String TypeMap
+unify Char Char _ = Right Map.empty
+unify (Char :->: Char) (Char :->: Char) _ = Right Map.empty
+unify (Var n0) (Var n1) _ = Right (Map.insert (max n0 n1) (SomeTypeRep (Var (min n0 n1))) Map.empty)
+unify (Var n) _ _ = Right (Map.insert n (SomeTypeRep Char) Map.empty)
+unify _ (Var n) _ = Right (Map.insert n (SomeTypeRep Char) Map.empty)
+unify a b _ = Left (("cannot unify " ++) . nestedShow a . (" against " ++) . nestedShow b $ "")
--- test/TestSkully/Internal/TypeRep.hs
+++ test/TestSkully/Internal/TypeRep.hs
@@ -4,18 +4,31 @@ module TestSkully.Internal.TypeRep (
 
 import Test.Hspec
 
+import qualified Data.IntMap.Strict as Map
 import Skully.Internal.TypeRep
 
 testUnify :: Spec
 testUnify =
-    describe "unify :: TypeRep a -> TypeRep b -> Either String (TypeRep a)" $ do
-        it "should unify Char against Char" $ unify Char Char `shouldBe` Right Char
+    describe "unify :: TypeRep a -> TypeRep b -> TypeMap -> Either String TypeMap" $ do
+        it "should unify Char against Char" $ unify Char Char Map.empty `shouldBe` Right Map.empty
         it "should not unify (Char :->: Char) against Char" $
-            unify (Char :->: Char) Char `shouldBe` Left "cannot unify (Char :->: Char) against Char"
+            unify (Char :->: Char) Char Map.empty `shouldBe` Left "cannot unify (Char :->: Char) against Char"
         it "should unify (Char :->: Char) against (Char :->: Char)" $
-            unify (Char :->: Char) (Char :->: Char) `shouldBe` Right (Char :->: Char)
+            unify (Char :->: Char) (Char :->: Char) Map.empty `shouldBe` Right Map.empty
         it "should not unify Char against (Char :->: Char)" $
-            unify Char (Char :->: Char) `shouldBe` Left "cannot unify Char against (Char :->: Char)"
+            unify Char (Char :->: Char) Map.empty `shouldBe` Left "cannot unify Char against (Char :->: Char)"
+        it "should unify (Var 0) against Char with (Var 0) ~ Char" $
+            unify (Var 0) Char Map.empty `shouldBe` Right (Map.insert 0 (SomeTypeRep Char) Map.empty)
+        it "should unify Char against (Var 0) with (Var 0) ~ Char" $
+            unify Char (Var 0) Map.empty `shouldBe` Right (Map.insert 0 (SomeTypeRep Char) Map.empty)
+        it "should unify (Var 0) against (Var 1) with (Var 1) ~ (Var 0)" $
+            unify (Var 0) (Var 1) Map.empty `shouldBe` Right (Map.insert 1 (SomeTypeRep (Var 0)) Map.empty)
+        it "should unify (Var 4) against (Var 2) with (Var 4) ~ (Var 2)" $
+            unify (Var 4) (Var 2) Map.empty `shouldBe` Right (Map.insert 4 (SomeTypeRep (Var 2)) Map.empty)
+        it "should unify (Var 66) against Char with (Var 66) ~ Char" $
+            unify (Var 66) Char Map.empty `shouldBe` Right (Map.insert 66 (SomeTypeRep Char) Map.empty)
+        it "should unify Char against (Var 78) with (Var 78) ~ Char" $
+            unify Char (Var 78) Map.empty `shouldBe` Right (Map.insert 78 (SomeTypeRep Char) Map.empty)
 
 testSkullyTypeRep :: Spec
 testSkullyTypeRep =
