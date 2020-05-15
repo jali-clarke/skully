5b2a9d355ce6e77394b5379ddcede654c5fc0264
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-05-15 20:48:48 +0000

    introduce :->:

--- src/Skully/Internal/TypeRep.hs
+++ src/Skully/Internal/TypeRep.hs
@@ -7,12 +7,16 @@ module Skully.Internal.TypeRep (
 
 data TypeRep a where
     Char :: TypeRep Char
+    (:->:) :: TypeRep a -> TypeRep b -> TypeRep (a -> b)
 
 instance Eq (TypeRep a) where
     Char == Char = True
+    _ == _ = False
 
 instance Show (TypeRep a) where
     show Char = "Char"
+    show _ = "Char :->: Char"
 
 unify :: TypeRep a -> TypeRep b -> Either String (TypeRep a)
 unify Char Char = Right Char
+unify _ _ = Left "cannot unify (Char :->: Char) against Char"
--- test/TestSkully/Internal/TypeRep.hs
+++ test/TestSkully/Internal/TypeRep.hs
@@ -10,6 +10,8 @@ testUnify :: Spec
 testUnify =
     describe "unify :: TypeRep a -> TypeRep b -> Either String (TypeRep a)" $ do
         it "should unify Char against Char" $ unify Char Char `shouldBe` Right Char
+        it "should not unify (Char :->: Char) against Char" $
+            unify (Char :->: Char) Char `shouldBe` Left "cannot unify (Char :->: Char) against Char"
 
 testSkullyTypeRep :: Spec
 testSkullyTypeRep =
