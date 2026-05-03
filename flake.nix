edfd7fe1bd90bd4fe81915ad04c1e3dde122a947
Jinnah Ali-Clarke <jinnah@noredink.com>
2026-05-03 00:49:30 -0400

    update for nixpkgs 25.11

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {self, nixpkgs, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs: [hpkgs.hspec]);
       in
       {
         devShell = pkgs.mkShell {
           name = "skully-dev-shell";
           buildInputs = [
             pkgs.cabal-install
-            pkgs.ghc
+            ghc
             pkgs.wget
           ];
         };
